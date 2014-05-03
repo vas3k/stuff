082035bc4a7de16873f8f8d63a803e7f2d195563
ReDetection <redetection@gmail.com>
2014-05-03 13:06:07 +0700

    using direct servisgid api, not yandex widget callback

--- python/parsers/megafon_msk.py
+++ python/parsers/megafon_msk.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-def megafon_msk_balance(login, password):
-    MEGAFON_API = "https://moscowsg.megafon.ru/WIDGET_INFO/GET_INFO?X_Username=%s&X_Password=%s&CHANNEL=WYANDEX&LANG_ID=1&P_RATE_PLAN_POS=1&P_PAYMENT_POS=2&P_ADD_SERV_POS=4&P_DISCOUNT_POS=3"
-    data = urllib2.urlopen(MEGAFON_API % (login, password)).read()
-    regex = re.compile(r'<div title=\\"Ваш баланс\\" class=\\"subs_balance balance_good\\">(.+?)<span>', re.UNICODE)
-    result = float(regex.findall(data)[0])
-    return round(result, 2)
+import urllib2
+from xml.etree import ElementTree
 
+def megafon_msk_balance(login, password):
+    MEGAFON_API = "https://moscowsg.megafon.ru/ROBOTS/SC_TRAY_INFO?X_Username=%s&X_Password=%s"
+    data = urllib2.urlopen(MEGAFON_API % (login, password)).read()
+    balance = ElementTree.fromstring(data).find("BALANCE").text
+    return float(balance)
